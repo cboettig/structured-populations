1c1a4f48bbac015678a01aff37c89189e79b436a
Carl Boettiger <cboettig@gmail.com>
2010-05-16 15:29:09 -0700

    fixing bugs in both crowley eqn and general formula

--- R/noise_approx.R
+++ R/noise_approx.R
@@ -28,51 +28,56 @@ twoD <- function(t, y, p){
 	# \dot y = \beta_1(x,y)
 	yd2 <- p["b2"] * y[2] * (p["K"] - y[1] - y[2]) - p["d2"] * y[2] - p["c2"] * y[1] * y[2]
 	# sigma_x^2
-	yd3 <-  2*( p["b1"] * (p["K"] - 2*y[1] - y[2]) - p["d1"] + p["c1"] * y[2] ) * y[3]	+		# 2 \partial_x \alpha_1(x,y)  * \sigma_x^2
-		(-p["b1"] * y[1]  + p["c1"] * y[1]  ) *  y[5]									+		# \partial_y \alpha_1(x,y)  * cov(x,y)
+	yd3 <-  2*( p["b1"] * (p["K"] - 2*y[1] - y[2]) - p["d1"] + p["c1"] * y[2] ) * y[3]	+		# 2 \partial_x \alpha_1(x,y)  * \sigma_x^2 +
+		2 * (-p["b1"] * y[1]  + p["c1"] * y[1]  ) *  y[5]								+		# 2 \partial_y \alpha_1(x,y)  * cov(x,y) +
 		p["b1"] * y[1] * (p["K"] - y[1] - y[2]) + p["d1"] * y[1] + p["c1"] * y[1] * y[2]		# \alpha_2
 	# sigma_y^2
-	yd4 <- 2*( p["b2"] * (p["K"] - y[1] - 2*y[2]) - p["d1"] + p["c2"] * y[1] ) * y[4]	+		# 2 \partial_y beta_1(x,y)  * \sigma_y^2
-		(-p["b2"] * y[2]  + p["c2"] * y[2]  ) * y[5]									+		# \partial_x \beta_1(x,y) * cov(x,y)
-		p["b1"] * y[1] * (p["K"] - y[1] - y[2]) + p["d1"] * y[1] + p["c1"] * y[1] * y[2]		# \beta_2
+	yd4 <- 2*( p["b2"] * (p["K"] - y[1] - 2*y[2]) - p["d2"] - p["c2"] * y[1] ) * y[4]	+		# 2 \partial_y beta_1(x,y)  * \sigma_y^2 +
+		2 * (-p["b2"] * y[2]  - p["c2"] * y[2]  ) * y[5]								+		# 2 \partial_x \beta_1(x,y) * cov(x,y) +
+		p["b2"] * y[1] * (p["K"] - y[1] - y[2]) + p["d2"] * y[1] + p["c2"] * y[1] * y[2]		# \beta_2
 	# cov 
-	yd5 <- (  2*( p["b1"] * (p["K"] - 2*y[1] - y[2]) - p["d1"] + p["c1"] * y[2] )  +			# (\partial_x \alpha_1 +
-		( p["b2"] * (p["K"] - y[1] - 2*y[2]) - p["d1"] + p["c2"] * y[1] ) ) * y[5] +			#   \partial_y \beta_1 )* cov 
-		(-p["b2"] * y[2]  + p["c2"] * y[2]  ) * y[3]	+										# \partial_x \beta_1 * sigma_x^2
+	yd5 <- (  (p["b1"] * (p["K"] - 2*y[1] - y[2]) - p["d1"] + p["c1"] * y[2] )			+		# (\partial_x \alpha_1 +
+		( p["b2"] * (p["K"] - y[1] - 2*y[2]) - p["d2"] - p["c2"] * y[1] ) ) * y[5]		+		# \partial_y \beta_1 )* cov + 
+		(-p["b2"] * y[2]  - p["c2"] * y[2]  ) * y[3]									+		# \partial_x \beta_1 * sigma_x^2 +
 		(-p["b1"] * y[1]  + p["c1"] * y[1]  ) * y[4]											# \partial_y \alpha_1 * sigma_y^2
-
 	list(c(yd1, yd2, yd3, yd4, yd5))
 }
-
-
-
-
-
 
 linnoise <- function(t,y,p, f, g, J){
 	n <- length(y)				# d states, d variances, (d^2-d)/2 covariances
 	d <- -3/2 + sqrt(9/4+2*n)	# dimension of the system
 	yd <- numeric(n)
-
 	jac <- J(t,y,p) 
-
 	# macroscopics
 	yd[1:d] <- f(t,y,p)
 
-	# variances
-	yd[ (d+1):(2*d) ] <- 2 * jac %*% y[ (d+1):(2*d) ] + g(t,y,p)
-	
-	# covariances
+
+	# build the variance-covariance matrix
+	M = matrix(0, d,d)
+	diag(M) = y[(d+1):(2*d) ]
 	k <- 2*d+1
 	for (i in 1:(d-1)){
 		for (j in (i+1):d){
-				# Computes ( J(i,i) + J(j,j) )*Cov(i,j)  + J(j,i)*Cov(i,i) + J(i,j) Cov(j,j)
-				yd[k] <- ( jac[i,i]+ jac[j,j])*y[k] + jac[j,i] * y[i+d] +  jac[i,j] * y[j+d]
-				k = k+1
+			M[i,j] <- y[k]
+			k <- k+1
 		}
 	}
-	list(yd);
+	
+	E <- jac %*% M
+	dM <- E + t(E) + diag(g(t,y,p) )
+
+	# unfold the matrix, consider cholskey decomposition!
+	yd[(d+1):(2*d) ] <- diag(dM) 
+	k <- 2*d+1
+	for (i in 1:(d-1)){
+		for (j in (i+1):d){
+			dM[i,j] -> yd[k]
+			k <- k+1
+		}
+	}
+	list(yd)
 }
+
 
 
 f_crowley <- function(t,y,p){
@@ -93,8 +98,8 @@ g_crowley <- function(t,y,p){
 
 
 J_crowley <- function(t,y,p){
-	j <- c(		( p["b1"] * (p["K"] - 2*y[1] - y[2]) - p["d1"] + p["c1"] * y[2] ),	(-p["b1"] * y[1]  + p["c1"] * y[1]  ), 
-			(-p["b2"] * y[2]  + p["c2"] * y[2]  ),								( p["b2"] * (p["K"] - y[1] - 2*y[2]) - p["d1"] + p["c2"] * y[1] ) ) 
+	j <- c(		( p["b1"]*(p["K"] - 2*y[1] - y[2]) - p["d1"] + p["c1"]*y[2] ),		(-p["b1"] * y[1]  + p["c1"] * y[1]  ), 
+			-(p["b2"] + p["c2"])*y[2],												( p["b2"] * (p["K"] - y[1] - 2*y[2]) - p["d2"] - p["c2"] * y[1] ) ) 
 	t(matrix(j,2,2))
 }
 
@@ -103,8 +108,9 @@ J_crowley <- function(t,y,p){
 
 k<-1000
 crowley_parameters <- c(b1=.11/k, b2=.6/k, d1=0.1, d2=.1, c1=0.1/k, c2=3.9/k, K=k)
-times <- seq(0,10,length=100)
-yo <- c(50, 450, 0, 0, 0) # (xo, yo, sigma_xo sigma_yo, cov)
+crowley_parameters <- c(b1=.2/k, b2=.6/k, d1=0.1, d2=.1, c1=0.1/k, c2=.2/k, K=k)
+times <- seq(0,500,length=500)
+yo <- c(200, 550, 0, 0, 0) # (xo, yo, sigma_xo sigma_yo, cov)
 
 
 o2 <- lsoda(yo, times, twoD, crowley_parameters)
@@ -112,6 +118,9 @@ o2 <- lsoda(yo, times, twoD, crowley_parameters)
 
 eqns <- function(t,y,p){ linnoise(t,y,p, f_crowley, g_crowley, J_crowley) }
 testcase <- lsoda(yo, times, eqns, crowley_parameters)
+
+
+jac <- J_crowley(0, yo, crowley_parameters)
 
 
 
