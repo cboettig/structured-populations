c8066ec0de73c621155d5c7efabff24aa86eaea7
Carl Boettiger <cboettig@gmail.com>
2010-05-16 21:54:11 -0700

    warning_signals.c now works with the crowley version of gillespie.c

--- src/pars.c
+++ src/pars.c
@@ -12,7 +12,7 @@ void * pars_alloc(int N, int K, double e, double a, double h, double start_pollu
 	my_pars->time_index = 0;
 	my_pars->hist_index = 0;
 	my_pars->checkpts[0] = 0;
-	my_pars->checkpts[1] = my_pars->start_polluting;
+	my_pars->checkpts[1] = start_polluting;
 	my_pars->start_polluting = start_polluting;
 	my_pars->pollute_rate = pollute_rate;
 	my_pars->pollute_increment = pollute_increment;
