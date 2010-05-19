0b6ce3ea1210284b71f3ab270b1ad6532ee0f31b
Carl Boettiger <cboettig@gmail.com>
2010-05-18 23:54:45 -0700

    added adult cannibalism on pupa

--- R/beetles.R
+++ R/beetles.R
@@ -1,11 +1,3 @@
-# y is given as  c( state, variances, covariances )  states = d, then length = d+d+(d^2-d)/2 = 1.5*d+d^2/2 = n
-#d <- 4
-#n <- 1.5*d+d^2/2
-
-
-
-## Beetle model parameters
-beetle_pars <- c(	b=5, ue= 0, ul = .001, up = 0, ua = .03, ae = 1/3.8, al = 1/(20.2-3.8), ap = 1/(25.5-20.2), cle = 0.01, cap = 0.004, cae = 0.01)
 
 ## Beetle model macroscopic eqns
 b_beetles <- function(t,y,p){
@@ -20,7 +12,7 @@ b_beetles <- function(t,y,p){
 d_beetles <- function(t,y,p){
 	f1 <-  (p["ue"] + p["cle"]*y[2] + p["cae"]*y[4] )*y[1]  
 	f2 <- p["ul"]*y[2]
-	f3 <- p["up"]*y[3] 
+	f3 <- (p["up"]+p["cap"]*y[4])*y[3] 
 	f4 <- p["ua"]*y[4] 
 	c(f1, f2, f3, f4)
 }
@@ -28,10 +20,11 @@ d_beetles <- function(t,y,p){
 ## Jacobian of f
 J_beetles <- function(t,y,p){
  t( matrix(c(
-	-(p["ue"]+p["cle"]*y[2]+p["cae"]*y[4]),	-p["cle"]*y[1],	0,	p["b"] - p["cae"]*y[1],
-	  0,			-p["ul"],	0,			0,
-	  0,				0,	-p["up"],		0,
-	  0,				0,		0,			-p["ua"] ), 4, 4))
+	-(p["ue"]+p["cle"]*y[2]+p["cae"]*y[4]),			-p["cle"]*y[1],	0,	p["b"] - p["cae"]*y[1],
+	  0,			-p["ul"],	0,						0,
+	  0,				0,	-p["up"]-p["cap"]*y[4],		-p["cap"]*y[3],
+	  0,				0,		0,						-p["ua"] 
+	  ), 4, 4))
 
 }
 
@@ -46,10 +39,12 @@ T_beetles <- function(t,y,p){
 }
 
 
+## Beetle model parameters
+beetle_pars <- c(	b=5, ue= 0, ul = .001, up = 0.00, ua = .003, ae = 1/3.8, al = 1/(20.2-3.8), ap = 1/(5.5-5.2), cle = 0.01, cap = 0.004, cae = 0.01)
 
 
 
-times <- seq(0,1000,length=100)
+times <- seq(0,7000,length=500)
 d <- 4
 n <- 1.5*d+d^2/2
 yo_beetles <- numeric(n)
@@ -60,20 +55,18 @@ beetle_data <- lsoda(yo_beetles, times, beetle_eqns, beetle_pars)
 
 
 
-D <- 4
-transition_matrix <- T_beetles(0, yo_beetles, beetle_pars)
-net_transitions <- sapply(1:D, function(i){ sum(transition_matrix[,i]-transition_matrix[i,]) } )
-
-
+png("beetles_noise.png")
 par(mfrow=c(2,1))
 m <- max(beetle_data[,2:5])
 plot(beetle_data[,1], beetle_data[,2], type = 'l', col="yellow", lwd=3, ylim=c(0,m), xlab="time", ylab="mean", cex.lab=1.3, main="Beetle ELPA model" )
 lines(beetle_data[,1], beetle_data[,3], col="yellowgreen", lwd=3)	
 lines(beetle_data[,1], beetle_data[,4], col="lightgreen", lwd = 3)	
 lines(beetle_data[,1], beetle_data[,5], col="darkgreen", lwd = 3)	
+legend("topright", c("egg", "larva", "pupa", "adult"), lty=1, col=c("yellow", "yellowgreen", "lightgreen", "darkgreen") )
 
 v <- max(sqrt(beetle_data[,6:9]))
 plot(beetle_data[,1], sqrt(beetle_data[,6]), type = 'l', col="yellow", lwd=3, ylim=c(0,v), xlab="time", ylab="stdev", cex=1.3 )
 lines(beetle_data[,1], sqrt(beetle_data[,7]), col="yellowgreen", lwd=3)	
 lines(beetle_data[,1], sqrt(beetle_data[,8]), col="lightgreen", lwd = 3)	
-lines(beetle_data[,1], sqrt(beetle_data[,9]), col="darkgreen", lwd = 3)	
+lines(beetle_data[,1], sqrt(beetle_data[,9]), col="darkgreen", lwd = 3)
+dev.off()
