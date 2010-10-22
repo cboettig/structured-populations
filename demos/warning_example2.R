6f2c00fe48278ac7dde98ce60ea18e409cfc02ed
Carl Boettiger <cboettig@gmail.com>
2010-10-21 18:14:39 -0700

    crash.png example in notebook

--- demos/warning_example2.R
+++ demos/warning_example2.R
@@ -9,7 +9,7 @@ T<- 40*25
 n_pts <- 201
 require(stochPop)
 pars = c(Xo = 730, e = 0.5, a = 100, K = 1000, h = 200, 
-    i = 0, Da = 0*.17, Dt = 0, p = 2)
+    i = 0, Da = .17, Dt = 0, p = 2)
 sn <- saddle_node_ibm(pars, times=seq(0,T, length=n_pts))
 X <- ts(sn$x1,start=sn$time[1], deltat=sn$time[2]-sn$time[1])
 
