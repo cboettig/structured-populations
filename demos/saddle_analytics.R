39a295ae0e3bde82af6800f8cf87f61f74726979
Carl Boettiger <cboettig@gmail.com>
2010-10-26 11:19:38 -0700

    added SWF animation and multiple root finding

--- demos/saddle_analytics.R
+++ demos/saddle_analytics.R
@@ -1,72 +1,53 @@
 # saddle_analytics.R
 params <- c(K=1000, e=.5, h=200, p=2)
 b <- function(x, params){ params["e"]*params["K"]*x^params["p"]/(x^params["p"]+params["h"]^params["p"]) }
-#d <- function(x, a, params){ params["e"]*x+a}
 
-d <- function(x, a, params) { a*(x-params["K"])+params["e"]*params["K"] }
+## "Changing Mean" Dynamics
+d <- function(x, a, params){ params["e"]*x+a}
+A<- seq(50,250, length=100)
+
+## "Constant Mean" Dynamics
+#d <- function(x, a, params) { a*(x-params["K"])+params["e"]*params["K"] }
+#A<- seq(1,0,length=100)
+
+x <- seq(0, 1300, by=5)
+
+require(rootSolve)
+xhat <- function(a, params, interval=c(0,1000)){
+		F <- function(x) {b(x, params) - d(x, a, params) }
+		uniroot.all(F, interval=interval)
+		
+	}
+	
+bifur <- function(){
+	for(a in A){
+		plot(x, b(x,params)-d(x,a, params), type="l", ylim=c(-150, 900), ylab="rate")
+		lines(x, b(x,params), lwd=4, lty=1, col="darkblue")
+		lines(x, d(x,a, params), lwd=4, lty=1, col="darkred")
+		abline(h=0, lwd=4, lty=2, col="darkgrey")
+		crit_pts <- sort(xhat(a, params))
+		print(crit_pts)
+		if(length(crit_pts) == 1){
+			points(crit_pts, 0, pch=19, cex=1.5)
+		} else if(length(crit_pts > 1)){
+			points(min(crit_pts), 0, cex=1.5)
+			points(max(crit_pts), 0, pch=19, cex=1.5)
+		}
+	}
+}
+require(animation)
+## GIF animation, loop=0 for infinite loop
+#saveMovie(bifur(), interval=.2, loop=0, outdir=getwd())
+
+## shockwave flash animation:
+saveSWF(bifur(), interval=.2, dev="png", swfname="bifur.swf", height=300, width=300, outdir=getwd())
+
+
+
+
+# "Changing mean dynamics"
 
 lambda <- function(x, a, params){ params["p"]*params["e"]*params["K"]*x^(params["p"]-1)/(x^params["p"]+params["h"]^params["p"]) - pparams["p"]*params["e"]*params["K"]*x^(2*params["p"]-1)/(x^params["p"]+params["h"]^params["p"])^2-params["e"] }
 
 
-x <- seq(0, 1300, by=5)
-a<- 1
-#par(mfrow=c(2,1))
-
-bifur <- function()
-for(a in seq(1,0,length=10)){
-plot(x, b(x,params)-d(x,a, params), type="l", ylim=c(-150, 900))
-lines(x, b(x,params), lwd=4, lty=1, col="darkblue")
-lines(x, d(x,a, params), lwd=4, lty=1, col="darkred")
-}
-
-require(animation)
-saveMovie(bifur(), interval=.5, loop=1, outdir=getwd())
-
-
-xhat <- function(a){
-	F <- function(x, a, params){abs(b(x, params) - d(x, a, params)) } 
-	nlm(F, 1000, a, params)$estimate
-}
-
-a <- seq(100, 300, length=500)
-X <- sapply(a, xhat)
-
-par(mar=c(5,4,4,5)+.1)
-plot(a, X, xlab="a", ylab="Pop Equilibrium", pch=17)
-par(new=TRUE)
-plot(a, lambda(X, a, params), xlab="", ylab="", pch=19, xaxt="n", yaxt="n", col="red")
-axis(4, col="red", col.ticks="red")
-mtext("lambda", side=4, line=3, col="red")
-legend("left",col=c("black","red"),pch=c(17,19),legend=c("Population","lambda"))
-abline(h=0, col="red")
-
-
-
-
-
-
-x <- 300:800
-plot(x, b(x,params)-d(x,174,params) )
-
-
-
-
-
-# The canonical form of the bifurcation is second order,
-# dx/dt = x^2 + a
-# and involves a symmetric approach.  
-xhat_left <- function(a){ 
-	f <- function(x){ x^2+a }
-	uniroot(f, c(-100,0))$root
-}
-xhat_right <- function(a){ 
-	f <- function(x){ x^2+a }
-	uniroot(f, c(0, 100))$root
-}
-
-a <- seq(-5,0,length=100)
-yl <- sapply(a, xhat_left)
-yr <- sapply(a, xhat_right)
-plot(c(a,a), c(yl,yr), pch=19, xlab="lambda", ylab="x_hat")
-
-
+## Uniroot.all makes the previous code here unnecssary 
