54dbdb578f5a91e2796ce2c097f4abeca8719d30
Carl Boettiger <cboettig@gmail.com>
2010-10-25 19:19:50 -0700

    animated graphic of bifurcation

--- demos/saddle_analytics.R
+++ demos/saddle_analytics.R
@@ -1,17 +1,26 @@
 # saddle_analytics.R
 params <- c(K=1000, e=.5, h=200, p=2)
 b <- function(x, params){ params["e"]*params["K"]*x^params["p"]/(x^params["p"]+params["h"]^params["p"]) }
-d <- function(x, a, params){ params["e"]*x+a}
+#d <- function(x, a, params){ params["e"]*x+a}
+
+d <- function(x, a, params) { a*(x-params["K"])+params["e"]*params["K"] }
 
 lambda <- function(x, a, params){ params["p"]*params["e"]*params["K"]*x^(params["p"]-1)/(x^params["p"]+params["h"]^params["p"]) - pparams["p"]*params["e"]*params["K"]*x^(2*params["p"]-1)/(x^params["p"]+params["h"]^params["p"])^2-params["e"] }
 
 
-x <- seq(0, 1000, by=5)
-a<- 100
+x <- seq(0, 1300, by=5)
+a<- 1
 #par(mfrow=c(2,1))
+
+bifur <- function()
+for(a in seq(1,0,length=10)){
 plot(x, b(x,params)-d(x,a, params), type="l", ylim=c(-150, 900))
 lines(x, b(x,params), lwd=4, lty=1, col="darkblue")
 lines(x, d(x,a, params), lwd=4, lty=1, col="darkred")
+}
+
+require(animation)
+saveMovie(bifur(), interval=.5, loop=1, outdir=getwd())
 
 
 xhat <- function(a){
