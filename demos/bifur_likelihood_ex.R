690abb2c92128d39b7c91720b64a339f0f5b2925
Carl Boettiger <cboettig@gmail.com>
2010-11-27 17:23:50 -0800

    added example code for saddle-node bifucation likelihood

---
+++ demos/bifur_likelihood_ex.R
@@ -0,0 +1,10 @@
+# bifur_likelihood_ex.R
+
+source("../R/likelihood_bifur_models.R")
+r<-10
+theta<-3
+beta<-1
+curve( -(x-theta)^2+r, 0, 10, ylim=c(-2, r+1))
+m <- init_sdemodel(pars = c(r=r, theta=theta, beta=beta), Xo = 10, model="SN")
+X <- simulate.sdemodel(m)
+plot(X)
