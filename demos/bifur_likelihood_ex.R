22045a9ddf12344a5bcf3e28c7d1788a130253c5
Carl Boettiger <cboettig@gmail.com>
2010-11-28 00:45:30 -0800

    autocommit

--- demos/bifur_likelihood_ex.R
+++ demos/bifur_likelihood_ex.R
@@ -2,10 +2,10 @@
 
 source("../R/likelihood_bifur_models.R")
 pars = c(r=10, theta=3, beta=2)
-m <- init_sdemodel(pars =pars, Xo = 6.2, model="SN", N=500)
+m <- init_sdemodel(pars =pars, Xo = 6.2, model="SN", N=200)
 X <- simulate.SN(m)
 # set the initial search values (to be other than the true ones)
-m$pars <- c(r=8,theta=2,beta=1)
+m$pars <- c(r=11,theta=1.5,beta=1)
 
 out <- update.SN(m, X)
 print(out$pars)
@@ -25,8 +25,9 @@ mtext("data",side=4,line=3)
 dev.off()
 
 gitcom <- system('git commit -a -m "autocommit"', intern=TRUE)
+# only first line of git commit commandline return will be printed in flickr discription
 system(paste('flickr_upload --tag="stochpop bifurcation"', '--description="', gitcom, '"',  ' saddle_node_fit.png'))
-
+system(paste('hpc-autotweets ', gitcom, ' done'))
 
 
 A <- seq(1,10, by=.1)
