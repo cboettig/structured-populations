33384396514d20a9fee781b0a5b1aab7f920afa5
Carl Boettiger <cboettig@gmail.com>
2010-11-28 13:49:02 -0800

    autocommit

--- demos/bifur_likelihood_ex.R
+++ demos/bifur_likelihood_ex.R
@@ -1,19 +1,7 @@
 # bifur_likelihood_ex.R
-
+## Figure plotting function
 source("../R/likelihood_bifur_models.R")
-pars = c(r=5, theta=3, beta=.1)
-m <- init_sdemodel(pars =pars, Xo = 8, model="SN", N=500, T=100)
-X <- simulate.SN(m)
-# set the initial search values (to be other than the true ones)
-m$pars <- c(r=21,theta=.5,beta=.01)
 
-out <- update.SN(m, X)
-print(out$pars)
-
-
-
-
-## Figure
 plotcurves <- function(m, out, pars, X, oucurve=NULL){ # m is the initial conditions model (input to update.SN), out the fit model (output of update.SN), pars the parameters used for the simulated data, X
 	curve( -(x-pars['theta'])^2+pars['r'], 0, 10, ylim=c(-2, r+1), lwd=3, main="true vs estimated model", col="darkgray")
 	curve( -(x-out$pars["theta"])^2+out$pars["r"], 0, 10,add=T, col="red", lty=2, lwd=3)
@@ -31,6 +19,17 @@ plotcurves <- function(m, out, pars, X, oucurve=NULL){ # m is the initial condit
 	mtext("data",side=4,line=3)
 }
 
+
+pars = c(r=5, theta=3, beta=.1)
+m <- init_sdemodel(pars =pars, Xo = 8, model="SN", N=500, T=100)
+X <- simulate.SN(m)
+# set the initial search values (to be other than the true ones)
+m$pars <- c(r=21,theta=.5,beta=.01)
+
+out <- update.SN(m, X)
+
+
+#plot 
 png("saddle_node_fit.png")
 plotcurves(m, out, pars, X)
 dev.off()
@@ -59,23 +58,23 @@ gitcom <- system('git commit -a -m "autocommit"', intern=TRUE)
 system(paste('flickr_upload --tag="stochpop bifurcation"', '--description="', gitcom, '"',  ' linear_fit.png'))
 
 
-# fit the quadratic model using the linear estimate as a starting point
-linearfit <- update.SN(linearmodel, X)
+## fit the quadratic model using the linear estimate as a starting point
+#linearfit <- update.SN(linearmodel, X)
 
 
 
 
 
 ## Plot the likelihood surface 
-A <- seq(1,10, length=200)
-l <- sapply(A, function(a){	
-	pars['theta'] <- a
-	SN.lik(X, pars)})
-png("likelihood_cross-section.png")
-plot(A, l, xlab="theta", ylab="-loglik")
-lines(A,l)
-dev.off()
-system('flickr_upload --tag="stochpop likelihood saddle-node" likelihood_cross-section.png')
+#A <- seq(1,10, length=200)
+#l <- sapply(A, function(a){	
+#	pars['theta'] <- a
+#	SN.lik(X, pars)})
+#png("likelihood_cross-section.png")
+#plot(A, l, xlab="theta", ylab="-loglik")
+#lines(A,l)
+#dev.off()
+#system('flickr_upload --tag="stochpop likelihood saddle-node" likelihood_cross-section.png')
 
 
 
