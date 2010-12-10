1b835b72be928eb3a5714c430646bad5fe830eb0
Carl Boettiger <cboettig@gmail.com>
2010-12-09 17:21:12 -0800

    power curve for N

--- warningsignals/demos/lin_bifur_models.R
+++ warningsignals/demos/lin_bifur_models.R
@@ -5,34 +5,37 @@ require(pmc)
 require(odesolve)
 
 ## Simulate a dataset under slow linear change
-pars <- c(Ro=50, m= -5, theta=1, sigma=1)
-X <- simulateGauss(timedep_LTC, pars, N=500, T=1)
 
-## fit both const and timedep models
-start <- c(Ro=.5, m=0, theta=.1, sigma=.1)
-timedep <- updateGauss(timedep_LTC, start, X, control=list(maxit=1000))
-start <- c(Ro=.5, theta=.1, sigma=.1)
-const <- updateGauss(const_LTC, start, X, control=list(maxit=1000))
+n <- seq(10,200, by=10)
+sfInit(parallel=TRUE, cpu=16)
+data <- sfLapply(n, 
 
-#update(timedep, simulate(timedep))
-#update(const, simulate(const))
+	function(i){
+	pars <- c(Ro=50, m= -5*.9, theta=1, sigma=1)
+	X <- simulateGauss(timedep_LTC, pars, N=N[i], T=10)
 
-out <- montecarlotest(const, timedep, cpu=16)
-save(list=ls(), file="lin_bifur_models.Rdat")
+	## fit both const and timedep models
+	start <- c(Ro=.5, m=0, theta=.1, sigma=.1)
+	timedep <- updateGauss(timedep_LTC, start, X, control=list(maxit=1000))
+	start <- c(Ro=.5, theta=.1, sigma=.1)
+	const <- updateGauss(const_LTC, start, X, control=list(maxit=1000))
 
-png("timeseries.png")
-plot(X)
-dev.off()
+	out <- montecarlotest(const, timedep, cpu=16)
+	save(list=ls(), file="lin_bifur_models.Rdat")
+	png("timeseries.png")
+	plot(X)
+	dev.off()
+	png("lin_bifur_models.png")
+	plot(out)
+	dev.off()
 
-png("lin_bifur_models.png")
-plot(out)
-dev.off()
+	id <- i 
+	gitcom <- system('git log -n -1', intern=TRUE)[[1]]
+	system(paste('flickr_upload --tag="stochpop warningsignals" --description="', gitcom,  '" lin_bifur_models.png timeseries.png', sep=""))
+	system(paste('hpc-autotweets "@cboettig #stochpop warningsignals done, id = ', id, gitcom, '"', sep=""))
 
-id <- 5
-gitcom <- system('git log -n -1', intern=TRUE)[[1]]
-system(paste('flickr_upload --tag="stochpop warningsignals" --description="', gitcom,  '" lin_bifur_models.png timeseries.png', sep=""))
-system(paste('hpc-autotweets "@cboettig #stochpop warningsignals done, id = ', id, gitcom, '"', sep=""))
-
+	out
+})
 
 
 
