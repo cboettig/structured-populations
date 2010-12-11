771d445648b9e80840494fef33fc047dbab9f1d4
Carl Boettiger <cboettig@gmail.com>
2010-12-10 18:50:32 -0800

    trying LSN

--- warningsignals/demos/lin_bifur_models.R
+++ warningsignals/demos/lin_bifur_models.R
@@ -14,13 +14,13 @@ sfLibrary(warningsignals)
 sfExportAll()
 
 	pars <- c(Ro=50, m= -.4, theta=1, sigma=1)
-	X <- simulateGauss(timedep_LTC, pars, N=50, T=1)
+	X <- simulateGauss(timedep_LSN, pars, N=50, T=1)
 
 	## fit both const and timedep models
 	start <- c(Ro=.5, m=0, theta=.1, sigma=.1)
-	timedep <- updateGauss(timedep_LTC, start, X, control=list(maxit=1000))
+	timedep <- updateGauss(timedep_LSN, start, X, control=list(maxit=1000))
 	start <- c(Ro=.5, theta=.1, sigma=.1)
-	const <- updateGauss(const_LTC, start, X, control=list(maxit=1000))
+	const <- updateGauss(const_LSN, start, X, control=list(maxit=1000))
 
 	out <- montecarlotest(const, timedep, cpu=2, nboot=50)
 #	save(list=ls(), file="lin_bifur_models.Rdat")
