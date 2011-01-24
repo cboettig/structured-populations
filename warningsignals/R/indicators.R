3bf14b73a9e352c8705ac4c78219da0f49e1436c
Carl Boettiger <cboettig@gmail.com>
2011-01-24 14:29:32 -0800

    adding statistical indicators

---
+++ warningsignals/R/indicators.R
@@ -0,0 +1,32 @@
+# indicators.R
+# simple functions to calculate statistics often used as leading indicators of a regime shift
+
+window_var <- function(X, windowsize=length(X)/2){
+	sapply(0:(length(X)-windowsize), function(i){
+		var(X[(i+1):(i+windowsize)]) 
+	})
+}
+
+
+window_mean <- function(X, windowsize=length(X)/2){
+	sapply(0:(length(X)-windowsize), function(i){
+		mean(X[(i+1):(i+windowsize)]) 
+	})
+}
+
+require(psych)
+window_skew <- function(X, windowsize=length(X)/2){
+	sapply(0:(length(X)-windowsize), function(i){
+		skew(X[(i+1):(i+windowsize)]) 
+	})
+}
+
+window_kurtosi <- function(X, windowsize=length(X)/2){
+	sapply(0:(length(X)-windowsize), function(i){
+		kurtosi(X[(i+1):(i+windowsize)]) 
+	})
+}
+
+
+
+
