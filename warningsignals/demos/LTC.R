b2c2cc0610d7893a5b94c47b6af4a381a2621a65
Carl Boettiger <cboettig@gmail.com>
2011-02-04 09:38:11 -0800

    autocommit

--- warningsignals/demos/LTC.R
+++ warningsignals/demos/LTC.R
@@ -1,30 +1,23 @@
 #indicator_vs_likelihood.R
 tags <- "warningsignals stochpop"
+cpu <- 16
 nboot <- 16
 require(socialR)
 require(warningsignals)
-sfInit(parallel=TRUE, cpu=16)
-nboot=160
-sfLibrary(warningsignals)
-
-
-pars <- c(Ro=5.0, m= -.02, theta=100, sigma=1)
-sfExportAll()
+#sfInit(parallel=TRUE, cpu=16)
+#sfLibrary(warningsignals)
+pars <- c(Ro=5.0, m= -.04, theta=100, sigma=1)
+#sfExportAll()
 ## Simulate a dataset under slow linear change
 warning <- simulateGauss(timedep_LTC, pars, N=500, T=100, Xo=100)
-no_warning <- simulateGauss(const_LTC, pars, N=500, T=100, Xo=100)
 
 # Likelihood Fits to each data-set and their relative model comparison  
 timedep <- updateGauss(timedep_LTC, pars, warning, control=list(maxit=1000))
 const <- updateGauss(const_LTC, pars, warning, control=list(maxit=1000))
 llik_warning <- 2*(loglik(timedep)-loglik(const))
 
-timedep_no <- updateGauss(timedep_LTC, pars, no_warning, control=list(maxit=1000))
-const_no <- updateGauss(const_LTC, pars, no_warning, control=list(maxit=1000))
-llik_nowarning <- 2*(loglik(timedep_no)-loglik(const_no))
-save(list=ls(), file="LTC.Rdat")
 
-out <- montecarlotest(const, timedep, cpu=16, nboot=nboot, GetParNames=FALSE)
+out <- montecarlotest(const, timedep, cpu=cpu, nboot=nboot, GetParNames=FALSE)
 save(list=ls(), file="LTC.Rdat")
 social_plot(plot(out), file="LTC.png", tag="warningsignal stochpop LTC", comment=paste("pars = ", pars))
 
