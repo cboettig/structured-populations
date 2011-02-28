64db9d695edda3861f080bada79e1bc29a59ad9e
Carl Boettiger <cboettig@gmail.com>
2011-02-28 15:45:03 -0800

    updated CaCO3 to try LSN

--- warningsignals/demos/CaCO3.R
+++ warningsignals/demos/CaCO3.R
@@ -16,16 +16,23 @@ require(limma)
 X <-avereps(X, ID=X[,1])
 pars <- c(Ro=5.0, m= -.04, theta=mean(X[,2]), sigma=sd(X[,2])*5*2)
 const_pars <- c(Ro=5.0, theta=mean(X[,2]), sigma=sd(X[,2])*5*2)
-const <- updateGauss(const_LTC, const_pars, X, control=list(maxit=1000))
-timedep <- updateGauss(timedep_LTC, pars, X, control=list(maxit=1000))
-llik_warning <- 2*(loglik(timedep)-loglik(const))
 
+## Fit a linearized transcritical bifurcation model
+#const <- updateGauss(const_LTC, const_pars, X, control=list(maxit=1000))
+#timedep <- updateGauss(timedep_LTC, pars, X, control=list(maxit=1000))
+
+## Fit the linearized saddle-node bifurcation model
+const <- updateGauss(const_LSN, const_pars, X, control=list(maxit=1000))
+timedep <- updateGauss(timedep_LSN, pars, X, control=list(maxit=1000))
+
+
+print(llik_warning <- 2*(loglik(timedep)-loglik(const)))
 sfInit(parallel=TRUE, cpu=cpu)
 sfLibrary(warningsignals)
 sfExportAll()
 out <- montecarlotest(const, timedep, cpu=cpu, nboot=nboot, GetParNames=FALSE)
 save(list=ls(), file="CaCO3.Rdat")
-social_plot(plot(out), file="CaCO3.png", tag="warningsignal stochpop LTC climatedata CaCO3")
+social_plot(plot(out), file="LTC_CaCO3.png", tag="warningsignal stochpop LTC climatedata CaCO3")
 
 
 
