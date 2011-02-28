1214685f6b888ef24dadd3248f36b6b41f0d30a2
Carl Boettiger <cboettig@gmail.com>
2011-02-28 13:42:04 -0800

    ready for first real data runs (fingers crossed)

--- warningsignals/R/gaussian_process.R
+++ warningsignals/R/gaussian_process.R
@@ -19,8 +19,8 @@ rc.gauss <- function(setmodel, n=1, x0, to, t1, pars){
 	rnorm(n, mean=P$Ex, sd=sqrt(P$Vx))
 }
 dc.gauss  <- function(setmodel, x, x0, to, t1, pars, log = FALSE){
-  P <- setmodel(x0, to, t1, pars)
-  dnorm(x, mean=P$Ex, sd=sqrt(P$Vx), log=log)
+	  P <- setmodel(x0, to, t1, pars)
+	  dnorm(x, mean=P$Ex, sd=sqrt(P$Vx), log=log)
 }
 pc.gauss  <- function(setmodel, x, x0, to, t1, pars, lower.tail = TRUE, log.p = FALSE){ 
   P <- setmodel(x0, to, t1, pars)
@@ -44,26 +44,30 @@ lik.gauss <- function(X, pars, setmodel){
 	    n <- length(X[,1])
 		times <- X[,1]
 		Y <- X[,2] ## data values in second column
+		
 		out <- -sum( dc.gauss(setmodel, Y[2:n], Y[1:(n-1)], to=times[1:(n-1)], t1=times[2:n], pars, log=TRUE) )
 	}
 	out
 }
 simulateGauss <- function(setmodel, pars, N=100, Xo = 1, T = 1, t0 = 0, times=NULL){
-	X <- numeric(N)
-	X[1] <- Xo
-## Return a timeseries if times not specified explicitly
+	## Return a timeseries if times not specified explicitly
 	if(is.null(times)){
-		delta_t <- (T-t0)/N
+		X <- numeric(N)
+		X[1] <- Xo
+	delta_t <- (T-t0)/N
 		times <- seq(t0, T, by=delta_t)
 		for(i in 1:(N-1) ){
-			X[i+1] <- rc.gauss(setmodel, 1, x0=X[i], t=times[i+1],t1=times[i+2], pars)
+			X[i+1] <- rc.gauss(setmodel, 1, x0=X[i], to=times[i+1],t1=times[i+2], pars)
 		}
 		out <- ts(X, start=t0, deltat=delta_t)
 	} else {
-		for(i in 1:(N-1) ){
-			X[i+1] <- rc.gauss(setmodel, 1, x0=X[i], t=times[i+1],t1=times[i+2], pars)
+		N <- length(times)
+		X <- numeric(N-1)
+		X[1] <- Xo
+		for(i in 1:(N-2) ){
+			X[i+1] <- rc.gauss(setmodel, 1, x0=X[i], to=times[i+1],t1=times[i+2], pars)
 		}
-		out <- data.frame(times=times, X=X)
+		out <- data.frame(times=times[1:(N-1)], X=X)
 	}
 	out
 }
--- warningsignals/demos/dakos.R
+++ warningsignals/demos/dakos.R
@@ -33,22 +33,34 @@ caco3 <- read.table("caco3.txt")
 caco3 <- data.frame("MYYrs"=-caco3$V1, "CaCO3"=caco3$V2)
 g_ca <- caco3$MYYrs > -39 & caco3$MYYrs < -32  # Data with collapse (for plot)
 p_ca <- caco3$MYYrs > -39 & caco3$MYYrs < -34  # Data used in warning signal
-
 X <- data.frame("time"=caco3$MYYrs[p_ca], "data"=caco3$CaCO3[p_ca])
-
-pars <- c(Ro=5.0, m= -.04, theta=mean(X[,2]), sigma=sd(X[,2]))
-const_pars <- c(Ro=5.0, theta=mean(X[,2]), sigma=sd(X[,2]))
-timedep <- updateGauss(timedep_LTC, pars, X, control=list(maxit=1000))
+# Rather annoying to have time backwards and negative, lets reverse this.
+X <- data.frame("time"=rev(X[,1] - min(X[,1])), "data"=rev(X[,2]))
+## Also really annoying that there are replicate values, luckily a quick averaging call will remove them. 
+require(limma)
+X <-avereps(X, ID=X[,1])
+pars <- c(Ro=5.0, m= -.04, theta=mean(X[,2]), sigma=sd(X[,2])*5*2)
+const_pars <- c(Ro=5.0, theta=mean(X[,2]), sigma=sd(X[,2])*5*2)
 const <- updateGauss(const_LTC, const_pars, X, control=list(maxit=1000))
+timedep <- updateGauss(timedep_LTC, pars, X, control=list(maxit=1000))
 llik_warning <- 2*(loglik(timedep)-loglik(const))
 
-## Incorrect time intervals
-myyrs <- caco3$MYYrs[dat]
-end=myyrs[1]; start=myyrs[length(myyrs)]
-X <- ts(caco3[dat,2],start=start,end=end, freq=length(dat)/(end-start))
+sfInit(parallel=TRUE, cpu=cpu)
+sfLibrary(warningsignals)
+sfExportAll()
+out <- montecarlotest(const, timedep, cpu=cpu, nboot=nboot, GetParNames=FALSE)
+save(list=ls(), file="dakos.Rdat")
+social_plot(plot(out), file="dakos.png", tag="warningsignal stochpop LTC climatedata CaCO3")
 
 
-plot(caco3[dat,1], caco3[dat,2])
+
+
+
+## Falsely pretend it has even time intervals
+#myyrs <- caco3$MYYrs[dat]
+#end=myyrs[1]; start=myyrs[length(myyrs)]
+#X <- ts(caco3[dat,2],start=start,end=end, freq=length(dat)/(end-start))
+#plot(caco3[dat,1], caco3[dat,2])
 
 
 
