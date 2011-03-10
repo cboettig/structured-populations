f31a718716574960f133a230c62440ec98d5af49
Carl Boettiger <cboettig@gmail.com>
2011-03-09 16:11:09 -0800

    deut data ready for action

--- warningsignals/demos/deut_data.R
+++ warningsignals/demos/deut_data.R
@@ -33,11 +33,11 @@ for(i in 1:4){
 
 for(i in 1:4){
 	data[[i]] <- dakos_data_processing(data[[i]])
-#	social_plot(plot(data[[i]]), tags=paste(tags, "glaciation", i), file="deutdata.png")
+	social_plot(plot(data[[i]]), tags=paste(tags, "glaciation", i), file="deutdata.png")
 }
 
 for(i in 1:4){
-	social_plot(plot_kendalls(data[[i]]), tag=tags)
+	social_plot(plot_kendalls(data[[i]]$X_ts), tag=tags)
 }
 
 save(list=ls(), file="deut_data.Rdat")
