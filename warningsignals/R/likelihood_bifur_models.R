d0d6ea02625ef46552c19c45b8305ec716319e7a
Carl Boettiger <cboettig@gmail.com>
2011-03-11 10:22:20 -0800

    E(X) fixed equation in constOU to relax to theta, not Xo

--- warningsignals/R/likelihood_bifur_models.R
+++ warningsignals/R/likelihood_bifur_models.R
@@ -65,7 +65,7 @@ setLTC <- function(Xo, to, t1, pars, R){
 
 constOU <- function(Xo, to, t1, pars){
 	Dt <- t1 - to
-	Ex <- Xo*(1 - exp(-pars["Ro"]*Dt)) + pars["theta"]*exp(-pars["Ro"]*Dt) 
+	Ex <- pars["theta"]*(1 - exp(-pars["Ro"]*Dt)) + Xo*exp(-pars["Ro"]*Dt) 
 	Vx <- 0.5*pars["sigma"]^2 *(1-exp(-2*pars["Ro"]*Dt))/pars["Ro"]
 	if(pars['Ro'] < 0 ) Vx = Inf 
 	if(pars['sigma'] < 0 ) Vx = Inf 
