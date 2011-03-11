5b40f308fad43471ba62fe2a6d76c8e40bb940e9
Carl Boettiger <cboettig@gmail.com>
2011-03-11 12:55:35 -0800

    Fixed tau dist -- was doing variance both times

--- warningsignals/R/tau_dist_montecarlo.R
+++ warningsignals/R/tau_dist_montecarlo.R
@@ -26,17 +26,17 @@ err_rates <- function(null_dist, test_dist, p=.05){
 	c(null_err=null_err, test_err=test_err)
 }
 
-plt_tau <- function(test_tau_dist, null_tau_dist, type){
+plt_tau <- function(test_tau_dist, null_tau_dist, indicator){
 	td <- density(test_tau_dist[1,])
 	nd <- density(null_tau_dist[1,])
-	plot(nd, main=paste("Kendall's Tau in ", type), lwd=1, col=rgb(0,0,1,1), xlim=c(-1,1))
+	plot(nd, main=paste("Kendall's Tau in ", indicator), lwd=1, col=rgb(0,0,1,1), xlim=c(-1,1))
 	polygon(nd$x, nd$y, col=rgb(0,0,1,.5), border=rgb(0,0,1,.5))
-	polygon(td$x, td$y, col=rgb(0,0,1,.5), border=rgb(1,0,0,.5))
+	polygon(td$x, td$y, col=rgb(1,0,0,.5), border=rgb(1,0,0,.5))
 
 	#lines(nd, col="blue", lwd=3)
 	legend("topright", c("test", "null"), pch=15, col=c("red", "blue"))
-	text(xshift(2), yshift(0), paste("fraction of test with p <0.05 is ", sum(test_tau_dist[2,] <.05)/length(null_tau_dist[2,])) )
-	text(xshift(2), yshift(10), paste("frac of null with p <0.05 is ", sum(null_tau_dist[2,] <.05)/length(null_tau_dist[2,])) )
+	text(xshift(5), yshift(5), paste("fraction of test with p <0.05 is ", sum(test_tau_dist[2,] <.05)/length(null_tau_dist[2,])), pos=4 )
+	text(xshift(5), yshift(10), paste("frac of null with p <0.05 is ", sum(null_tau_dist[2,] <.05)/length(null_tau_dist[2,])), pos=4 )
 }
 
 
@@ -99,6 +99,8 @@ plot_kendalls <- function(warning){
 tau_dist_montecarlo <- function(X, const, timedep, signal=c("Variance", "Autocorrelation"), nboot=200, cpu=2){
 	print( llik_warning_fit <- 2*(loglik(timedep)-loglik(const)) )
 
+	observed_acor <- warning_stats(X, window_autocorr)
+	observed_var <- warning_stats(X, window_var)
 
 	if(cpu>1 & !sfIsRunning()){ 	
 		sfInit(parallel=TRUE, cpu=cpu)
@@ -110,17 +112,23 @@ tau_dist_montecarlo <- function(X, const, timedep, signal=c("Variance", "Autocor
 
 ## Look at the distribution of Taus
 	test_tau_dist <- sfSapply(1:nboot, function(i){
-		X <- simulate(timedep)
-		warning_stats(X, window_var)
+		Z <- simulate(timedep)
+		if(signal=="Variance"){  out <- warning_stats(Z, window_var)
+		} else if (signal=="Autocorrelation") { out <- warning_stats(Z, window_autocorr) 
+		} else { message("signal type not recognized")  }
+		out
 	})
 ## check that this is returning a matrix, not vector
 	null_tau_dist <- sfSapply(1:nboot, function(i){
 		Y <- simulate(const)
-		warning_stats(Y, window_var)
+		if(signal=="Variance"){  out <- warning_stats(Y, window_var)
+		} else if (signal=="Autocorrelation") { out <- warning_stats(Y, window_autocorr) 
+		} else { message("signal type not recognized")  }
+		out
 	})
 	out <- list(test_tau_dist=test_tau_dist, null_tau_dist=null_tau_dist,
 		signal=signal, X=X, llik_warning_fit=llik_warning_fit,
-		const=const, timedep=timedep)
+		const=const, timedep=timedep, observed_var = observed_var, observed_acor = observed_acor)
 	class(out) <- "tau_dist_montecarlo"
 	out
 }
@@ -134,7 +142,9 @@ plot.tau_dist_montecarlo <- function(out, show_sample=FALSE){
 		plt_data(warning, no_warning)
 	} else {
 		plt_tau(out$test_tau_dist, out$null_tau_dist, out$signal)
-		lines(v=out$llik_warning_fit, type=3, lwd=3, col="darkred")
+		if(out$signal == "Variance") observed_tau <- out$observed_var
+		if(out$signal == "Autocorrelation") observed_tau <- out$observed_acor
+		abline(v=observed_tau[1], lty=2, lwd=3, col="darkred")
 	}
 }
 
