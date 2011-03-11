ffc017b937678e877936f3b05fa2610fdf8f93f8
Carl Boettiger <cboettig@gmail.com>
2011-03-11 11:34:58 -0800

    running deut3.R example

--- warningsignals/demos/deut3.R
+++ warningsignals/demos/deut3.R
@@ -7,7 +7,7 @@ nboot <- 16
 load("deut_data.Rdat")
 
 X <- data[[3]]$X_ts
-
+social_plot(plot.dakos(data[[3]]), file="plot_dakos.png", tags=tags)
 # consider smarter estimate for Ro
 const_pars <- c(Ro=1/max(time(X)), theta=mean(X), sigma=sd(X))
 
@@ -18,8 +18,9 @@ pars <- c(Ro=as.numeric(const$pars["Ro"]), m=0, theta=as.numeric(const$pars["the
 #out <- heteroOU(X,pars)
 #fit <- updateGauss("heteroOU", pars, X, control=list(maxit=1000))
 
-
 timedep <- updateGauss(timedep_LTC, pars, X, control=list(maxit=1000))
+
+save(list=ls(), file="deut3.Rdat")
 
 ## Fit the linearized saddle-node bifurcation model
 #const <- updateGauss(const_LSN, const_pars, X, control=list(maxit=1000))
@@ -37,8 +38,8 @@ tau_var <- tau_dist_montecarlo(X, const, timedep, signal="Variance", nboot=nboot
 tau_acor <- tau_dist_montecarlo(X, const, timedep, signal="Autocorrelation", nboot=nboot, cpu=cpu)
 
 save(list=ls(), file="deut3.Rdat")
-social_plot(plot(tau_var), file="taudist_var.png", tags=paste(tags, "tau var"), mention="cboettig")
-social_plot(plot(tau_acor), file="taudist_acor.png", tags=paste(tags, "tau acor"))
+social_plot(plot(tau_var), file="taudist_var.png", tags=paste(tags, "tau var deut3"), mention="cboettig")
+social_plot(plot(tau_acor), file="taudist_acor.png", tags=paste(tags, "tau acor deut3"))
 
 # plot example data
 social_plot(plot(tau_var, show_sample=TRUE), tags="warningsignals stochpop tau deut3")
