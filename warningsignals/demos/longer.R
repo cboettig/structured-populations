aaaaaa96469d890a52747b02404afe9e81897f3c
Carl Boettiger <cboettig@gmail.com>
2011-03-14 13:28:52 -0700

    autocommit

--- warningsignals/demos/longer.R
+++ warningsignals/demos/longer.R
@@ -10,7 +10,7 @@ require(warningsignals)
 pars <- c(Ro=5.0, m= -.015, theta=100, sigma=1)
 const_pars <- c(Ro=5.0, theta=100, sigma=1)
 ## Some initial data: Simulate some sample data under slow linear change 
-X <- simulateGauss(timedep_LTC, pars, N=500, T=500, Xo=100)
+X <- simulateGauss(timedep_LTC, pars, N=50, T=100, Xo=100)
 
 # initialize and fit models (could just use updateGauss instead of generic
 #const <- updateGauss(const_LTC, const_pars, X, control=list(maxit=1000))  
@@ -22,15 +22,15 @@ tau_var <- tau_dist_montecarlo(X, const, timedep, signal="Variance", nboot=nboot
 tau_acor <- tau_dist_montecarlo(X, const, timedep, signal="Autocorrelation", nboot=nboot, cpu=cpu)
 
 save(list=ls(), file="longer.Rdat")
-social_plot(plot(tau_var), file="taudist_var.png", tags="warningsignals stochpop tau var simulation", comment="N=500, T=500, R=5-0.015*t", mention="cboettig")
-social_plot(plot(tau_acor), file="taudist_acor.png", tags="warningsignals stochpop tau acor simulation", comment="N=500, T=500, R=5-0.015*t")
+social_plot(plot(tau_var), file="taudist_var.png", tags="warningsignals stochpop tau var simulation", comment="N=50, T=100, R=5-0.015*t", mention="cboettig")
+social_plot(plot(tau_acor), file="taudist_acor.png", tags="warningsignals stochpop tau acor simulation", comment="N=50, T=100, R=5-0.015*t")
 
 
 ## MONTECARLO Non-parametric bootstrap using the exact values.  
 ## As noted above, in real data we would use the MLEs, which has the point-estimate problem.  
 out <- montecarlotest(const, timedep, cpu=cpu, nboot=nboot, GetParNames=FALSE)
 save(list=ls(), file="longer.Rdat")
-social_plot(plot(out), file="longer_mc.png", tag="warningsignal stochpop LTC, simulation", comment="N=500, T=500, R=5- 0/05*t")
+social_plot(plot(out), file="longer_mc.png", tag="warningsignal stochpop LTC, simulation", comment="N=50, T=100, R=5- 0/05*t")
 
 
 
