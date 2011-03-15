821dd06b3cae8233280da4722d5aed2e95bbd84e
Carl Boettiger <cboettig@gmail.com>
2011-03-15 14:32:41 -0700

    faster rate of change in simulated example, trying for more power in MC test

--- warningsignals/demos/indicator_vs_likelihood.R
+++ warningsignals/demos/indicator_vs_likelihood.R
@@ -1,16 +1,22 @@
 #indicator_vs_likelihood.R
 rm(list=ls()) ## start with clean workspace
-tags <- "warningsignals stochpop"
 nboot <- 1600
 cpu <- 16
 require(socialR)
 require(warningsignals)
 
 ########################### Begin actual analysis ######################## 
-pars <- c(Ro=5.0, m= -.015, theta=100, sigma=1)
+pars <- c(Ro=5.0, m= -.03, theta=100, sigma=1)
 const_pars <- c(Ro=5.0, theta=100, sigma=1)
 ## Some initial data: Simulate some sample data under slow linear change 
 X <- simulateGauss(timedep_LTC, pars, N=500, T=100, Xo=100)
+
+T <- max(time(X))
+N <- length(X)
+Xo <- X@.Data[1]
+sampling <- c(T=T, N=N, Xo=Xo)
+comment <- paste(c(names(pars), ":", pars, "\\", names(sampling), ":", sampling), collapse=" ")
+
 
 # initialize and fit models (could just use updateGauss instead of generic
 #const <- updateGauss(const_LTC, const_pars, X, control=list(maxit=1000))  
@@ -22,15 +28,15 @@ tau_var <- tau_dist_montecarlo(X, const, timedep, signal="Variance", nboot=nboot
 tau_acor <- tau_dist_montecarlo(X, const, timedep, signal="Autocorrelation", nboot=nboot, cpu=cpu)
 
 save(list=ls(), file="indicator_vs_likelihood.Rdat")
-social_plot(plot(tau_var), file="taudist_var.png", tags="warningsignals stochpop tau var")
-social_plot(plot(tau_acor), file="taudist_acor.png", tags="warningsignals stochpop tau acor")
+social_plot(plot(tau_var), file="taudist_var.png", tags="warningsignals stochpop tau var", comment=comment)
+social_plot(plot(tau_acor), file="taudist_acor.png", tags="warningsignals stochpop tau acor", comment=comment)
 
 
 ## MONTECARLO Non-parametric bootstrap using the exact values.  
 ## As noted above, in real data we would use the MLEs, which has the point-estimate problem.  
 out <- montecarlotest(const, timedep, cpu=cpu, nboot=nboot, GetParNames=FALSE)
 save(list=ls(), file="indicator_vs_likelihood.Rdat")
-social_plot(plot(out), file="indicator_vs_likelihood_mc.png", tag="warningsignal stochpop LTC")
+social_plot(plot(out), file="indicator_vs_likelihood_mc.png", tag="warningsignals stochpop MC LTC", comment=comment)
 
 
 
