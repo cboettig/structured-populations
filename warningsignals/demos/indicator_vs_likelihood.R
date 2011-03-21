46d3a964a2a3bf7f5a9a7d5d9ed3a9dd580b5a37
Carl Boettiger <cboettig@gmail.com>
2011-03-21 15:10:06 -0700

    another LSN example

--- warningsignals/demos/indicator_vs_likelihood.R
+++ warningsignals/demos/indicator_vs_likelihood.R
@@ -1,5 +1,5 @@
 #indicator_vs_likelihood.R
-nboot <- 16
+nboot <- 160
 cpu <- 16
 require(socialR)
 require(warningsignals)
@@ -7,10 +7,10 @@ gitcommit()
 
 
 ########################### Begin actual analysis ######################## 
-pars <- c(Ro=5.0, m= -.04, theta=100, sigma=1)
+pars <- c(Ro=5.0, m= -.05, theta=100, sigma=1)
 const_pars <- c(Ro=5.0, theta=100, sigma=1)
 ## Some initial data: Simulate some sample data under slow linear change 
-X <- simulateGauss(timedep_LTC, pars, N=50, T=100, Xo=100)
+X <- simulateGauss(timedep_LSN, pars, N=50, T=100, Xo=100)
 
 T <- max(time(X))
 N <- length(X)
@@ -23,7 +23,7 @@ comment <- paste(c(names(pars), ":", pars, "\\", names(sampling), ":", sampling,
 # initialize and fit models (could just use updateGauss instead of generic
 #const <- updateGauss(const_LTC, const_pars, X, control=list(maxit=1000))  
 const <- updateGauss(constOU, const_pars, X, control=list(maxit=1000))  
-timedep <-updateGauss(timedep_LTC, pars, X, control=list(maxit=1000)) 
+timedep <-updateGauss(timedep_LSN, pars, X, control=list(maxit=1000)) 
 
 # Tau approach comparison
 tau_var <- tau_dist_montecarlo(X, const, timedep, signal="Variance", nboot=nboot, cpu=cpu)
@@ -33,14 +33,14 @@ tau_kurtosis <- tau_dist_montecarlo(X, const, timedep, signal="Kurtosis", nboot=
 
 social_plot(plot(tau_var), file="taudist_var.png", tags="warningsignals stochpop tau var", comment=comment)
 social_plot(plot(tau_acor), file="taudist_acor.png", tags="warningsignals stochpop tau acor", comment=comment)
-social_plot(plot(tau_skew), file="taudist_var.png", tags="warningsignals stochpop tau skew", comment=comment)
-social_plot(plot(tau_kurtosis), file="taudist_acor.png", tags="warningsignals stochpop tau kurtosis", comment=comment)
+social_plot(plot(tau_skew), file="taudist_skew.png", tags="warningsignals stochpop tau skew", comment=comment)
+social_plot(plot(tau_kurtosis), file="taudist_kurtosis.png", tags="warningsignals stochpop tau kurtosis", comment=comment)
 
 
 ## MONTECARLO Non-parametric bootstrap using the exact values.  
 ## As noted above, in real data we would use the MLEs, which has the point-estimate problem.  
 out <- montecarlotest(const, timedep, cpu=cpu, nboot=nboot, GetParNames=FALSE)
-social_plot(plot(out), file="indicator_vs_likelihood_mc.png", tag="warningsignals stochpop MC LTC", comment=comment)
+social_plot(plot(out), file="indicator_vs_likelihood_mc.png", tag="warningsignals stochpop MC LSN", comment=comment)
 
 
 
