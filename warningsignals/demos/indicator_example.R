d989c5cc1618eac6d39cf3ce11e7980d0f261762
Carl Boettiger <cboettig@gmail.com>
2011-03-22 14:22:36 -0700

    autocommit

--- warningsignals/demos/indicator_example.R
+++ warningsignals/demos/indicator_example.R
@@ -1,8 +1,12 @@
 # indicator_example.R
 
-source("load_CaCO3.R")
-source("bootstrap_indicators.R")
+#source("load_CaCO3.R")
+#source("bootstrap_indicators.R")
 
-m<-fit_models(X, "LTC")
-social_plot(bootstrap_indicators(X, m$const, m$timedep, nboot=1000), file="bootstrap_tau.png")
+#m<-fit_models(X, "LTC")
+require(warningsignals)
+require(socialR)
+load("5550699471.Rdat")
+
+social_plot(bootstrap_indicators(X, m$const, m$timedep, nboot=1000), file="bootstrap_tau.png", width=480, height=4*480)
 
