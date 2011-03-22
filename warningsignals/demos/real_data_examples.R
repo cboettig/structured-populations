1114b716211770b1596eb966f6cc668e82977de6
Carl Boettiger <cboettig@gmail.com>
2011-03-22 12:00:55 -0700

    autocommit

--- warningsignals/demos/real_data_examples.R
+++ warningsignals/demos/real_data_examples.R
@@ -1,17 +1,33 @@
-
 source("load_CaCO3.R")
-social_plot(all_indicators(X), tags=tags)
+social_plot(all_indicators(X), tags=tags, file="CaCO3.png")
 
 source("load_deut.R")
 ## Glaciation I, processed
-social_plot(all_indicators(data[[1]]$X_ts), tags=tags)
+social_plot(all_indicators(data[[1]]$X_ts), tags=tags, file="DeutI.png")
 ## Glaciation II, processed
-social_plot(all_indicators(data[[2]]$X_ts), tags=tags)
+social_plot(all_indicators(data[[2]]$X_ts), tags=tags, file="DeutII.png")
 ## Glaciation III, processed
-social_plot(all_indicators(data[[3]]$X_ts), tags=tags)
+social_plot(all_indicators(data[[3]]$X_ts), tags=tags, file="DeutIII.png")
 ## Glaciation IV, processed
-social_plot(all_indicators(data[[4]]$X_ts), tags=tags)
+social_plot(all_indicators(data[[4]]$X_ts), tags=tags, file="DeutIV.png")
 
+
+source("load_deut.R")
+## Glaciation I, unprocessed
+social_plot(all_indicators(data[[1]]$raw_ts), tags=tags)
+## Glaciation II, unprocessed
+social_plot(all_indicators(data[[2]]$raw_ts), tags=tags)
+## Glaciation III, unprocessed
+social_plot(all_indicators(data[[3]]$raw_ts), tags=tags)
+## Glaciation IV, unprocessed
+social_plot(all_indicators(data[[4]]$raw_ts), tags=tags)
+
+
+
+
+
+
+##### should be able to pass raw time series as such, but no luck...
 ## Glaciation I, unprocessed
 social_plot(all_indicators(data[[1]]$X), tags=tags)
 ## Glaciation II, unprocessed
