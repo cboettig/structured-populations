0d583e1327de0775286c1c63065408f5b0df7c63
Carl Boettiger <cboettig@gmail.com>
2011-03-22 10:48:22 -0700

    added example of simple indicator

--- warningsignals/demos/indicator_example.R
+++ warningsignals/demos/indicator_example.R
@@ -1,20 +1,13 @@
-#indicator_vs_likelihood.R
-nboot <- 800
+# indicator_example.R
+# Run with a selected set of data from indicator_vs_likelihood.R simulation
+load("5550742684.Rdat")
+
+
+nboot <- 80
 cpu <- 16
 require(socialR)
 require(warningsignals)
 gitcommit()
-
-
-########################### Begin actual analysis ######################## 
-pars <- c(Ro=5.0, m= -.004999, theta=500, sigma=5)
-const_pars <- c(Ro=5.0, theta=500, sigma=5)
-## Some initial data: Simulate some sample data under slow linear change 
-X <- simulateGauss(timedep_LSN, pars, N=100, T=100, Xo=500)
-
-## grab some info for reporting
-T <- max(time(X)); N <- length(X); Xo <- X@.Data[1]; sampling <- c(T=T, N=N, Xo=Xo)
-comment <- paste(c(names(pars), ":", pars, "\\", names(sampling), ":", sampling, "nboot:", nboot, "LSN"), collapse=" ")
 
 ## traditional stats on this
 social_plot(all_indicators(X), file="indicators.png", tags="warningsignals stochpop tau indicators", comment=comment)
@@ -34,12 +27,6 @@ social_plot(plot(tau_var), file="taudist_var.png", tags="warningsignals stochpop
 social_plot(plot(tau_acor), file="taudist_acor.png", tags="warningsignals stochpop tau acor", comment=comment)
 social_plot(plot(tau_skew), file="taudist_skew.png", tags="warningsignals stochpop tau skew", comment=comment)
 social_plot(plot(tau_kurtosis), file="taudist_kurtosis.png", tags="warningsignals stochpop tau kurtosis", comment=comment)
-
-
-## MONTECARLO Non-parametric bootstrap using the exact values.  
-## As noted above, in real data we would use the MLEs, which has the point-estimate problem.  
-#out <- montecarlotest(const, timedep, cpu=cpu, nboot=nboot, GetParNames=FALSE)
-#social_plot(plot(out), file="indicator_vs_likelihood_mc.png", tag="warningsignals stochpop MC LSN", comment=comment)
 
 
 
