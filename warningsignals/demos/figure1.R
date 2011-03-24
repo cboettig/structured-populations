ec82bfb19a09021ee49c65c83987ed80eca45098
Carl Boettiger <cboettig@gmail.com>
2011-03-23 22:56:21 -0700

    autocommit

--- warningsignals/demos/figure1.R
+++ warningsignals/demos/figure1.R
@@ -6,12 +6,11 @@ source("load_CaCO3.R")
 CaCO3 <- X
 source("load_deut.R")
 deut3 <- data[[3]]$X_ts
-load("5550863240.Rdat") # consider replacing with a LSN sim
-sim_LTC <- X
+load("5555038554.Rdat")
 
 ## FIGURE 1
 social_plot(
-	all_indicators( list(sim_LTC, CaCO3, deut3), indicators = c("Variance", "Autocorrelation") ),
+	all_indicators(list(deteriorating=deteriorating, constant=constant, deuterium=deut3), indicators = c("Variance", "Autocorrelation") ),
 	tags="warningsignals stochpop"
 	)
 
