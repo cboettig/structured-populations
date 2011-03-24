46ce78481e1ba7a50815f08d040568499ba06041
Carl Boettiger <cboettig@gmail.com>
2011-03-23 17:24:48 -0700

    more cleanup

--- warningsignals/demos/figure1.R
+++ warningsignals/demos/figure1.R
@@ -3,10 +3,12 @@ source("load_CaCO3.R")
 CaCO3 <- X
 source("load_deut.R")
 deut3 <- data[[3]]$X_ts
+load("5550863240.Rdat") # consider replacing with a LSN sim
+sim_LTC <- X
 
 ## FIGURE 1
 social_plot(
-	all_indicators( list(CaCO3, deut3), indicators = c("Variance", "Autocorrelation", "Skew", "Kurtosis") ),
+	all_indicators( list(sim_LTC, CaCO3, deut3), indicators = c("Variance", "Autocorrelation") ),
 	tags="warningsignals stochpop"
 	)
 
--- warningsignals/demos/figure2.R
+++ warningsignals/demos/figure2.R
@@ -1,13 +1,24 @@
 ## FIGURE 2
+
+
+## load datafiles
 source("load_CaCO3.R")
 CaCO3 <- X
 source("load_deut.R")
 deut3 <- data[[3]]$X_ts
+load("5550863240.Rdat") # consider replacing with a LSN sim
+simLTC <- X
+
+
+## bootstrap each 
+simLTC_m<-fit_models(simLTC, "LTC")
+simLTC_taus <- bootstrap_tau(simLTC_m$X, simLTC_m$const, simLTC_m$timedep, indicators = c("Variance", "Autocorrelation"), nboot=160, cpu=16)
+
 
 CaCO3_m <- fit_models(CaCO3, "LSN")
-CaCO3_taus <- bootstrap_tau(CaCO3_m$X, CaCO3_m$const, CaCO3_m$timedep)
+CaCO3_taus <- bootstrap_tau(CaCO3_m$X, CaCO3_m$const, CaCO3_m$timedep, indicators = c("Variance", "Autocorrelation"), nboot=160, cpu=16)
 deut3_m <- fit_models(deut3, "LSN")
-deut3_taus <- bootstrap_tau(deut3_m$X, deut3_m$const, deut3_m$timedep)
+deut3_taus <- bootstrap_tau(deut3_m$X, deut3_m$const, deut3_m$timedep, indicators = c("Variance", "Autocorrelation"), nboot=160, cpu=16)
 
 save(list=ls(), file="real_data_examples.Rdat")
 
