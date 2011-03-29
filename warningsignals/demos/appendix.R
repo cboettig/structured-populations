a2ad38b3aac62673daf61b349d51a8a4ab40f3e7
Carl Boettiger <cboettig@gmail.com>
2011-03-29 11:15:27 -0700

    autocommit

--- warningsignals/demos/appendix.R
+++ warningsignals/demos/appendix.R
@@ -2,15 +2,13 @@
 source("load_CaCO3.R")
 CaCO3 <- X
 source("load_deut.R")
-source("../R/indicators.R")
 indicators <- c("Variance", "Autocorrelation", "Skew", "Kurtosis")
 ## FIGURE 1
 social_plot(
 	all_indicators(list(CaCO3=X, 
 				        GlaciationI=data[[1]]$X_ts, 
 				        GlaciationII=data[[2]]$X_ts ),	
-				   indicators=indicators, method="kendall",
-                   cex.axis=1.5, cex.lab=1.5),
+				   indicators=indicators, method="kendall"),
 	tags="warningsignals stochpop", 
 	height=length(indicators)*240, width=3*480
 )
