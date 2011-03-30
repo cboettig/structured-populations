337f7ff4977ab333b1595c347cf4d6d3dd735660
Carl Boettiger <cboettig@gmail.com>
2011-03-30 11:28:21 -0700

    minor update

--- warningsignals/demos/deut_likelihood.R
+++ warningsignals/demos/deut_likelihood.R
@@ -1,7 +1,8 @@
 load("5571632581.Rdat")
 require(warningsignals)
 require(socialR)
-
+nboot <- 8
+cpu <- 8
 
 mc <- lapply(1:3, function(i){
 	montecarlotest(m[[i]]$const, m[[i]]$timedep, nboot=nboot, cpu=cpu, GetParNames=FALSE)
