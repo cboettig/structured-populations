109aef83363abf9f7d9e6cc9416c91fa03d62947
Carl Boettiger <cboettig@gmail.com>
2011-03-30 17:15:25 -0700

    autocommit

---
+++ warningsignals/demos/parameter_dist.R
@@ -0,0 +1,59 @@
+load("35563325713.Rdat")
+require(socialR)
+require(warningsignals)
+
+
+plot_m <- function(pow, X=NULL){
+    test <- pow$test_par_dist
+    shade_plot <- function(par, lower=-Inf, upper=Inf){
+# disgard nonsense values
+        par <- par[par>lower & par < upper]
+        dist <- density(par)
+        plot(dist, type="n")
+        polygon(dist$x, dist$y, col="gray", border="darkgray")
+    }
+    upper <- 1
+    lower <- -1
+    if(!is.null(X)){
+        lower <- -10*length(X)/max(time(X))
+        upper <- -lower
+    }
+    shade_plot(test[2,], lower, upper)
+}
+
+
+par(mfrow=c(1,2))
+plot(density(deterior_mc$test_par_dist[2,], bw=1), xlim=c(-20,1))
+plot(density(constant_mc$test_par_dist[2,c(-173,-1717)]))
+
+
+## parameter distributions
+par_names <- c("Ro", "m", "theta", "sigma")
+plot_parameters <- function(pow){
+    test <- pow$test_par_dist
+    null <- pow$null_par_dist
+
+    shade_plot <- function(par, lower=-Inf, upper=Inf, ...){
+        par <- par[par>lower & par < upper]
+        dist <- density(par)
+        plot(dist, type="n", ...)
+        polygon(dist$x, dist$y, col="gray", border="darkgray")
+    }
+
+    n <- dim(test)[1]
+    par(mfrow=c(2,n))
+    for(i in 1:n ){
+        shade_plot(test[i,], main=par_names[i])
+    }
+    m <- dim(null)[1]
+    for(i in 1:m ){
+        if(i==2) plot(test[2,], type="n")
+        shade_plot(null[i,], main="")
+    }
+}
+
+social_plot(plot_parameters(deterior_mc), tag="sim deterior warningsignals stochpop parameters")
+social_plot(plot_parameters(constant_mc), tag="sim constant warningsignals stochpop parameters")
+social_plot(plot_parameters(deut3_mc), tag="sim deut3 warningsignals stochpop parameters") 
+
+
