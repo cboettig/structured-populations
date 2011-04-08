b017696906d41305dae177b17fa665519ced4a61
Carl Boettiger <cboettig@gmail.com>
2011-04-08 16:52:36 -0700

    autocommit

--- warningsignals/demos/ibm_sims.R
+++ warningsignals/demos/ibm_sims.R
@@ -1,7 +1,7 @@
 # Simulate a dataset from the full individual, nonlinear model
 T<- 1000
 n_pts <- 40
-require(stochPop)
+require(warningsignals)
 pars = c(Xo = 730, e = 0.5, a = 100, K = 1000, h = 200, 
     i = 0, Da = .09, Dt = 0, p = 2)
 sn <- saddle_node_ibm(pars, times=seq(0,T, length=n_pts))
