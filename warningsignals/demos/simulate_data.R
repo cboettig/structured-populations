dc49e4c6fc71107d9b4a0d68b0b04e62cb30ed7b
Carl Boettiger <cboettig@gmail.com>
2011-04-12 21:28:27 -0700

    autocommit

--- warningsignals/demos/simulate_data.R
+++ warningsignals/demos/simulate_data.R
@@ -10,7 +10,7 @@ const_pars <- c(Ro=5.0, theta=500, sigma=5)
 deteriorating <- simulateGauss(timedep_LSN, pars, N=100, T=100, Xo=500)
 
 ## estimate pars from the simulated data, for as close a sim as possible
-const_pars <- updateGauss(constOU, const_pars, X, control=list(maxit=1000))$pars
+const_pars <- updateGauss(constOU, const_pars, deteriorating, control=list(maxit=1000))$pars
 constant <- simulateGauss(constOU, const_pars, N=100, T=100, Xo=500)
 
 # put both datasets into a list and plot side-by-side
